97c3833dfbc911180fb2287310a1444cc1ff6b68
Guilherme Rodrigues <guilodron7@gmail.com>
2020-07-18 20:16:28 -0300

    Updates

---
+++ precache-manifest.51a17fa05a36056e42de8e0ffde3a4b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4907c0526ac397255b7b9b53c22a6a84",
+    "url": "/desafio_instabuy/index.html"
+  },
+  {
+    "revision": "847f8aac8ec0d78db944",
+    "url": "/desafio_instabuy/static/js/2.dafc29b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/desafio_instabuy/static/js/2.dafc29b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8784f072b3cd1f4b255",
+    "url": "/desafio_instabuy/static/js/main.a586fc8a.chunk.js"
+  },
+  {
+    "revision": "5de629f6763507aa6d6d",
+    "url": "/desafio_instabuy/static/js/runtime-main.53d2e221.js"
+  },
+  {
+    "revision": "316b1f4571d7e8dccc0918d3668edcc1",
+    "url": "/desafio_instabuy/static/media/logo-desktop.316b1f45.png"
+  }
+]);
